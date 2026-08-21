4ab3f695840d353a2bde65a547def5a31f200d83
kalaipriyan2006 <kalaipriyan2006@users.noreply.github.com>
2026-08-21 09:03:42 +0530

    Normalize Vercel API route paths

--- api/[...path].ts
+++ api/[...path].ts
@@ -9,5 +9,8 @@ export default async function handler(req: Request, res: Response) {
   }
 
   const app = await appPromise;
+  if (req.url && !req.url.startsWith('/api')) {
+    req.url = `/api${req.url}`;
+  }
   return app(req, res);
 }
